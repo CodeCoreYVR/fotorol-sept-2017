510f8a6c09f55e8b29c7176da5f65d35d5b796e5
Steve Godin <steve.god@gmail.com>
2017-10-04 12:32:38 -0700

    Add bodyParser, create form and post, add about page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 // The Express is returned as function and we can use
 // to create a web application by calling it.
 // 👇 creates an instance of a Express web app server.
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs')
 
 // morgan is a package for creating middleware functions that log
 // information about your app's requests and responses.
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('dev'))
 // Middleware functions are called in order of appearance in the
 // code. This one happens before our hello world below.
@@ -34,11 +36,34 @@ app.use((request, response, next) => {
 
 app.get('/', (request, response) => {
 
+  // Form data is available as an object on the property `request.body`
+  // if you've setup `body-parser` middleware.
+  console.log(request.body)
+
   // response.render will render template a file from the `/views`
   // directory as the content the response to the client.
   // Specify file by it path skipping `/views` and disregarding
   // its extension.
-  response.render('index')
+  response.render('index', {content: null})
+})
+
+app.post('/', (request, response) => {
+  // Form data is available as an object on the property `request.body`
+  // if you've setup `body-parser` middleware.
+  console.log(request.body)
+
+  const {body} = request;
+  // 👆 syntax sugar for 👇
+  // const body = request.body;
+
+  // response.render can take a second argument. It's an object where
+  // all of its properties will be available as local variables inside of
+  // of the rendered template.
+  response.render('index', body)
+})
+
+app.get('/about', (request, response) => {
+  response.render('about')
 })
 
 app.get('/hello-world', (request, response) => {
