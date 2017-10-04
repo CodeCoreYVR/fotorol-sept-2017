3b59956e6ea46a3502071aa88372a62900f2a3cc
Steve Godin <steve.god@gmail.com>
2017-10-04 10:46:22 -0700

    Setup debugging script for Node

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/', (request, response) => {
   // The `response` object represents the reply that our
   // server is going to send to the client.
 
+  // When running node with the `--inspect` command,
+  // you open a Chrome console and click on the Node logo in the
+  // upper-left corner of the console to open a dedicated
+  // debugger for Node. Anywhere you write the keyword, `debugger`
+  // in you code, the debugger will pause program and you'll
+  // be able to inspect the state of your program.
+  // debugger
   response.send(`Hello, Class!`)
 })
 
